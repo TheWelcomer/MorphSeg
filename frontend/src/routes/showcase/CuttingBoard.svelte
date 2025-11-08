faafac16e7c693cbcb5ad48d9a0346c3c8d744fc
block36underscore <block36not33@gmail.com>
2025-11-08 17:15:40 -0500

    split words

--- frontend/src/routes/showcase/CuttingBoard.svelte
+++ frontend/src/routes/showcase/CuttingBoard.svelte
@@ -1,19 +1,59 @@
 <script>
+  const Stage = {
+    BEFORE: 0,
+    WORDS: 1,
+  };
+
   let input = $state("What the Segma?");
+  let stage = $state(Stage.BEFORE);
+  let words = $state(splitWords(input));
   
+  function splitWords(text) {
+    return text.split(" ")
+               .map(word=>word + " ");
+  }
+
   function segment(text) {
     return text.split(" ").map((word)=>word.split(""));
   }
 
   export function startAnimation(){
-    alert("animation yay")
+    words = splitWords(input);
+    console.log(input);
+    console.log(words);
+    stage = Stage.WORDS;
   }
-</script> 
+</script>
 
 <div id=cutting-board>
-<input value={input} id=cutting-board-input/>
+  {#if stage == Stage.BEFORE}
+    <input id=cutting-board-input bind:value={input} />
+  {:else if stage == Stage.WORDS}
+    <div id="word-container">
+      {#each words as word}
+        <span class="word">
+          {word}
+        </span>
+      {/each}
+    </div>
+  {/if}
 </div>
 
 <style>
   @import "./style.css";
+  
+  #cutting-board {
+    align-items: center;
+    justify-content: center;
+    display: flex;
+  }
+
+  #word-container {
+    width: 100%;
+  }
+
+  .word {
+    display: inline-block;
+    background-color: blue;
+  }
 </style>
