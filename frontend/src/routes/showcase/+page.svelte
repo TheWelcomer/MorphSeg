35aeec32afb94bf95e6ce995813f0c75a60914e9
block36underscore <block36not33@gmail.com>
2025-11-08 14:21:43 -0500

    very basic prototype

---
+++ frontend/src/routes/showcase/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  let input = $state("What the Segma?");
+  let output = $state("")
+
+  function segment(text) {
+    return text.split("");
+  }
+
+</script>
+
+<input value={input} style="background: red"/>
+<br>
+<button onclick={()=>{output = segment(input)}}>
+  #mybutton
+</button>
+<br>
+Segmented: {output}
+
