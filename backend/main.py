efb6b74b6d40acbc7011ca648db4172c0127195f
Donald Winkelman <dmwink.tx@gmail.com>
2025-11-09 03:44:14 -0500

    BACKEND WORKSgaagaa

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
+from testmorphseg import MorphemeSegmenter
 
 app = FastAPI()
 
 @app.get("/")
 async def home_root():
-    return {"message": "success"}
+    return {"message": "success, is that all you got?"}
 
-@app.get("/deploy")
+@app.get("/health")
 async def home_root():
-    return {"message": "deploy success"}
+    return {"message": "healthy"}
+
+@app.get("/seg_list/{string}")
+async def seg_list(string: str):
+    morpheme_segmenter = MorphemeSegmenter(lang="eng", train_from_scratch=False)
+    segments = morpheme_segmenter.segment(string, output_string=False)
+    print(segments)
+    return {"message": segments}
+
+@app.get("/seg_string/{string}")
+async def seg_string(string: str):
+    morpheme_segmenter = MorphemeSegmenter(lang="eng", train_from_scratch=False)
+    segments = morpheme_segmenter.segment(string, output_string=True)
+    print(segments)
+    return {"message": segments}
