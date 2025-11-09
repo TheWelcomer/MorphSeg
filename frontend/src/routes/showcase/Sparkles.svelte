492346186fa71ac24763d72f7ff3525140e5e52d
Taoran Ye <taoranyeduck@gmail.com>
2025-11-08 23:33:58 -0500

    :spor::kle::spor::kle::knife:

---
+++ frontend/src/routes/showcase/Sparkles.svelte
@@ -0,0 +1,30 @@
+<script>
+    var numbkle = new Array(20);
+    let sparc = "/assets/Sprite-Sparkle.gif";
+</script>
+
+<style>
+    #sparklid {
+        background-color: transparent;
+        height: 100%;
+        width: 100%;
+        float: left;
+        position: fixed;
+    }
+    .sparkle {
+        image-rendering: pixelated;
+        position: absolute;
+        width: 10vmax;
+        height: 10vmax;
+    }
+    #cooked {
+        font-size: 10em;
+    }
+</style>
+
+<div id="sparklid">
+    {#each numbkle as _, numb}
+        <img class="sparkle" src={sparc} alt="sparkle!" style="left: {Math.random()*110-5}%; top: {Math.random()*110-5}%;">
+    {/each}
+    <div id="cooked">ALL COOKED!</div>
+</div>
