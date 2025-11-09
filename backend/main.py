12ee83cbadc18e2959226e0e544a21a92e0ac1c7
Donald Winkelman <dmwink.tx@gmail.com>
2025-11-09 00:20:21 -0500

    backend up more

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,16 @@ app = FastAPI()
 
 @app.get("/")
 async def home_root():
-    return {"message": "success"}
+    return {"message": "success, is that all you got?"}
 
-@app.get("/deploy")
+@app.get("/health")
 async def home_root():
-    return {"message": "deploy success"}
+    return {"message": "healthy"}
+
+@app.get("/seg_list/{string}")
+async def seg_list(string: str):
+    return {"message": "seg_list success"}
+
+@app.get("/seg_string/{string}")
+async def seg_string():
+    return {"message": "seg_string success"}
